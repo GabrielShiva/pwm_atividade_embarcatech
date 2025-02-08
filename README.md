436f871fa1ac100e499d6c9687901d4bf964ec96
Gabriel Shiva Gomes Oliveira <gabrielshivagomes@gmail.com>
2025-02-08 19:16:38 -0300

    adiciona readme

---
+++ README.md
@@ -0,0 +1,50 @@
+# Controle de Servo Motor via PWM com Raspberry Pi Pico
+
+Este projeto faz parte do capítulo 7 do curso **Embarcatech Unity 4**, onde exploramos o controle de um servo motor utilizando PWM (Modulação por Largura de Pulso) com a placa **Raspberry Pi Pico**. O código foi desenvolvido em C e utiliza a biblioteca `pico-sdk` para configurar e controlar o servo motor.
+
+## Descrição do Projeto
+
+O objetivo deste projeto é demonstrar como controlar um servo motor utilizando PWM. O servo motor é controlado por pulsos de largura variável, onde a largura do pulso determina a posição do eixo do motor. O código implementado permite que o servo motor se mova entre três posições principais: 0°, 90° e 180°, e também realiza um movimento contínuo entre essas posições.
+
+## Configuração do Hardware
+
+- **Raspberry Pi Pico**: Microcontrolador utilizado para gerar os sinais PWM.
+- **Servo Motor**: Conectado ao pino GPIO 12 (ou 22, conforme definido no código).
+- **LED Azul (opcional)**: Conectado ao pino GPIO 12 para testes visuais.
+
+## Configuração do Software
+
+O código foi desenvolvido utilizando a linguagem C e a biblioteca `pico-sdk`. Abaixo estão as principais configurações e funções implementadas:
+
+### Definições e Constantes
+
+- **SERVO_PIN**: Define o pino GPIO conectado ao servo motor (ou LED para testes).
+- **PULSE_PERIOD_US**: Período do pulso PWM em microssegundos (20ms para servo motores padrão).
+- **PWM_CLK_DIVISER**: Divisor de clock para ajustar a frequência do PWM.
+- **PWM_WRAP**: Valor máximo do contador PWM.
+
+### Funções Principais
+
+- **set_position**: Calcula a largura do pulso ativo com base no tempo fornecido e aplica esse valor ao canal PWM.
+- **pwm_setup**: Configura o PWM para o pino especificado, definindo o divisor de clock, o valor máximo do contador e habilitando o PWM.
+
+### Funcionamento do Código
+
+1. **Inicialização**: O sistema de I/O é inicializado e o PWM é configurado para operar a 50Hz.
+2. **Posicionamento do Servo**: O servo motor é movido para três posições distintas (0°, 90° e 180°) com um intervalo de 5 segundos entre cada movimento.
+3. **Movimento Contínuo**: Após os movimentos iniciais, o servo motor começa a se mover continuamente entre 0° e 180° em incrementos de 5μs, com um intervalo de 10ms entre cada movimento.
+
+## Como Utilizar
+
+Para vizualizar na placa BITDOGLAB, descomente a linha que define a GPIO 12 e comente a linha que define a GPIO 22
+
+### Compilação e Upload
+1. Clone o repositório utilizando o comando abaixo.
+```
+git clone https://github.com/GabrielShiva/pwm_atividade_embarcatech.git
+```
+2. No VsCode, clique na extensão `Raspberry Pi Pico W`, depois em *Importar Projeto*, selecione a pasta do projeto, escolha a versão do SDK (2.1.0) e, por fim, em *Importar*.
+
+## Vídeo de Apresentação
+
+Assita ao vídeo com a apresentação do projeto (clicando aqui)[#].
--- src/main.c
+++ src/main.c
@@ -1,14 +1,65 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/pwm.h"
 
+// #define SERVO_PIN 12 // define o pino conectado ao LED azul para teste na placa BITDOGLAB
+#define SERVO_PIN 22 // define o pino conectado ao servo motor
 
+#define PULSE_PERIOD_US 20000 // período do pulso em microsegundos
 
-int main()
-{
-    stdio_init_all();
+const float PWM_CLK_DIVISER = 100.0;
+const uint16_t PWM_WRAP = 24999;
+static uint channel = {0};
+static uint slice = {0};
+
+// calcula a largura de pulso (ativo) para o tempo (μs) fornecido, e aplica este valor para o slice (canal) fornecido
+void set_position(uint slice, uint channel, uint pulse_us) {
+    uint pulse_width = (pulse_us * PWM_WRAP) / PULSE_PERIOD_US;
+    pwm_set_chan_level(slice, channel, pulse_width);
+}
+
+// configuração do PWM para o canal
+void pwm_setup() {
+    gpio_set_function(SERVO_PIN, GPIO_FUNC_PWM); // define o pino conectado ao servo como pwm
+    slice = pwm_gpio_to_slice_num(SERVO_PIN); // obtem o slice PWM referente ao pino conectado ao servo
+    channel = pwm_gpio_to_channel(SERVO_PIN); // obtem o canal PWM do pino 22
+
+    pwm_set_clkdiv(slice, PWM_CLK_DIVISER); // define o divisor de clock do PWM (50Hz)
+    pwm_set_wrap(slice, PWM_WRAP); // define o valor máximo do contador PWM
+    pwm_set_enabled(slice, true);
+}
+
+int main() {
+    stdio_init_all(); // inicializa o sistema padrão I/O
+    pwm_setup(); // configuração do PWM para uma frequência de 50Hz
+
+    // Define a posição de 180 graus
+    set_position(slice, channel, 2400);
+    sleep_ms(5000); // define atraso de 5 segundos para esperar na posição
+
+    // Define a posição de 90 graus
+    set_position(slice, channel, 1470);
+    sleep_ms(5000); // define atraso de 5 segundos para esperar na posição
+
+    // Define a posição de 0 graus
+    set_position(slice, channel, 500);
+    sleep_ms(5000); // define atraso de 5 segundos para esperar na posição
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        // define a posição para 0 graus (500μs) e incrementa este valor em 5μs enquanto não for maior que 2400μs (180 graus).
+        // caso não seja maior que 2400, chama a função set_position para definir a posição do servo motor 
+        for (int pulse_active = 500; pulse_active <= 2400; pulse_active = pulse_active + 5) { 
+            set_position(slice, channel, pulse_active);
+            sleep_ms(10); // define atraso de 10 milisegundos para esperar na posição
+        }
+
+        // define a posição para 180 graus (2400μs) e decrementa este valor em 5μs enquanto não for menor que 500μs (0 graus).
+        // caso não seja menor que 500, chama a função set_position para definir a posição do servo motor 
+        for (int pulse_active = 2400; pulse_active >= 500; pulse_active = pulse_active - 5) {
+            set_position(slice, channel, pulse_active);
+            sleep_ms(10); // define atraso de 10 milisegundos para esperar na posição
+        }
     }
+
+    return 0;
 }
